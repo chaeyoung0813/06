4b39264782c8d263a457fd07c982dbbfdeb00081
chaeyoung0813 <chaeyoung0813@naver.com>
2022-10-11 17:33:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,21 +3,46 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-
-int square(int n)
-{
-	return (n*n);
+int get_integer (){
+    int n;
+   printf("n을 입력하시오 : ");
+    scanf("%d",&n);
+    return (n);
 }
+
+int get_integer2 (){
+    int r;
+   printf("r을 입력하시오: ");
+    scanf("%d",&r);
+    return (r);
+}
+
+int factorial (int n){
+    int res = 1,i;
+    for (i=1;i<=n;i++)
+    res =res*i;
+    return (res);
+}
+
+int combination ()
+{
+   int n,r;
+   n=get_integer();
+   r=get_integer2();
+   int a,b,c;
+   a=factorial (n);
+   b=factorial (r);
+   c=factorial (n-r);
+    return (a/(b*c));
+}
+
 
 int main(int argc, char *argv[]) 
 
 {
-	int a=2;
-	a=square(a);
-	
-	printf("a=%i\n", a);
-	
-	return 0;
+	int x;
+    x = combination ();
+    printf("%d",x);
+
 	
 }
